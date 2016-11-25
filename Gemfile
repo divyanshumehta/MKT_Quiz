377cb7c7db583335e0c0b75b304dadffb8f3bc02
divyanshumehta <divyanshumehta@outlook.com>
2016-11-25 18:37:58 +0530

    Added fb lofin

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'sprockets-rails'
 gem 'rails', '~> 5.0.0'
 # Use sqlite3 as the database for Active Record
 gem 'pg'
+gem 'omniauth-facebook'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,40 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
-before_create :init
 
 
-	def init
-		self.lvl=1
-		self.score=0
-	end
+  before_create :init
+
+
+    def init
+      self.lvl=1
+      self.score=0
+    end
+
+  def self.find_for_facebook_oauth(auth)
+    user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.lvl=1
+      user.score=0
+      #user.image = auth.info.image # assuming the user model has an image
+    end
+    
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 
 end
+
